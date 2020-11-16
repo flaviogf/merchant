9deafd1992d44691b80650ddcf701b793188c2f1
flaviogf <flavio.fernandes6@gmail.com>
2020-11-16 13:37:48 -0300

    feat: adding random quotes

--- src/Merchant/MerchantLogger.cs
+++ src/Merchant/MerchantLogger.cs
@@ -5,6 +5,24 @@ namespace Merchant
 {
     internal class MerchantLogger : ILogger
     {
+        private static readonly Random _random = new Random();
+
+        private static readonly string[] _quotes = new string[]
+        {
+            "What're ya buyin?",
+            "What're ya sellin'?",
+            "Not enough cash, stranger!",
+            "Got some rare things on sale, stranger!",
+            "A wise choice, mate! Its ammo will penetrate just about anything!",
+            "Ah, the choice of an avid gun collector! It's a nice gun, mate!",
+            "Stranger, stranger, stranger... now that's a weapon!",
+            "Not only will you need cash, but you'll need GUTS to buy that weapon!",
+            "I see you have an eye for things. Gun's not just about shoot'n, i'ts about reload'n! You'll know what I'm talkin' about!",
+            "Is that all, stranger?",
+            "Heh heh heh... Thank you!",
+            "Ah! I'll buy it at a high price!"
+        };
+
         private readonly string _name;
 
         private readonly IWriter _writer;
@@ -22,7 +40,12 @@ namespace Merchant
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            var title = $"Got some rare things on sale, stranger 💥";
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+
+            var title = NextQuote();
 
             var subtitle = $"{Enum.GetName(typeof(LogLevel), logLevel).ToLower()}: {_name}";
 
@@ -34,6 +57,13 @@ namespace Merchant
         public bool IsEnabled(LogLevel logLevel)
         {
             return logLevel == LogLevel.Error;
+        }
+
+        private string NextQuote()
+        {
+            var index = _random.Next(0, _quotes.Length);
+
+            return _quotes[index];
         }
     }
 }
